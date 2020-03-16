b5cc85d26ea08522a9538d29ec38ae781a769476
Neikon <Neikon@users.noreply.github.com>
2020-03-16 22:20:11 +0100

    Add files via upload
    codigo del programa para que se ejecute al inicio

---
+++ Ejecutar al inicio codigo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ejecutar_al_inicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory+ @"NeiSpotlight.exe");
+             using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+             {
+
+                 //key.SetValue("NeiSpotlight", AppDomain.CurrentDomain.BaseDirectory + @"NeiSpotlight.exe");
+                 key.DeleteValue("NeiSpotlight", false);
+
+             }
+
+        }
+    }
+}
