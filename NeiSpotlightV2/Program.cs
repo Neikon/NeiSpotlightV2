d83c45245a054c10d0b4e482fcb40624b23d0c8c
Neikon <neikon@hotmail.es>
2020-03-26 17:02:45 +0100

    ahora si la foto a poner como fondo de pantalla ya se ha usado eligira aleatoriamente otra de la carpeta neispotligh

--- NeiSpotlightV2/Program.cs
+++ NeiSpotlightV2/Program.cs
@@ -16,11 +16,15 @@ namespace NeiSpotlightV2
         [DllImport("User32", CharSet = CharSet.Auto)]
         public static extern int SystemParametersInfo(int uiAction, int uiParam,
         string pvParam, uint fWinIni);
+
         static void Main(string[] args)
         {
+            
+
             //creamos las rutas para el usuario que esta ejecutando el programa
             string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
             string destPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + @"\NeiSpotlight";
+
             //obtenemos el ancho de la pantalla
             var screenWidth = Screen.PrimaryScreen.Bounds.Width;
             //creamos nuestra carpeta donde guardad las fotos que vamos usando
@@ -44,10 +48,61 @@ namespace NeiSpotlightV2
                     }
                 }
             }
+
+            
+
             //De nuestra carpeta elegiremos la imagen mas reciente 
             var wallpaper = new DirectoryInfo(destPath).GetFiles().OrderByDescending(f => f.LastWriteTime).First();
+
+            if (SeUso(wallpaper))
+            {
+                
+                int cantidadDeFotos = new DirectoryInfo(destPath).GetFiles().Length;
+
+                var numRand = new Random();
+                
+                wallpaper = new DirectoryInfo(destPath).GetFiles().OrderByDescending(f => f.LastWriteTime).ElementAt( numRand.Next(cantidadDeFotos) );
+                
+            }
+            
+            EscribirLog(wallpaper.Name);
             //Ponemos como wallpaper nuestra imagen
             SystemParametersInfo(0x0014, 0, wallpaper.FullName, 0x0001);
+            
+        }
+
+        public static bool SeUso(FileInfo wallpaper) {
+
+            if (wallpaper.Name == ComprobarLog())
+            {
+                return true;
+            }
+            else
+            {
+                
+                return false;
+            }
+            
+        }
+
+        public static string ComprobarLog()
+        {
+            string logPath = AppDomain.CurrentDomain.BaseDirectory + @"\log.txt";
+            
+            if (System.IO.File.Exists(logPath))
+            {
+              return  System.IO.File.ReadAllText(logPath);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static void EscribirLog(string cadena)
+        {
+            string logPath = AppDomain.CurrentDomain.BaseDirectory + @"\log.txt";
+            System.IO.File.WriteAllText(logPath,cadena);
         }
     }
 }
